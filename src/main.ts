a76ffc14431f9e6ea04d7e008ed99f65131cc72d
arunakcm <arunak.comdemetrics@gmail.com>
2024-12-24 12:52:51 +0530

    main file change

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,31 @@
-// import { ValidationPipe } from '@nestjs/common';
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   // Enable global validation using class-validator
-//   app.useGlobalPipes(new ValidationPipe());
-//   app.enableCors();
-//   await app.listen(process.env.PORT ?? 3000);
-// }
-
-// bootstrap();
-
-
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
 
 async function bootstrap() {
-  const server = express();
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  app.enableCors(); // Enable CORS for serverless compatibility
-  await app.init();
-  
-  return server;
+  const app = await NestFactory.create(AppModule);
+  // Enable global validation using class-validator
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  await app.listen(process.env.PORT ?? 3000);
 }
 
-// Export the server for Vercel to use
-export const handler = bootstrap();
+bootstrap();
+
+
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ExpressAdapter } from '@nestjs/platform-express';
+// import * as express from 'express';
+
+// async function bootstrap() {
+//   const server = express();
+//   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+//   app.enableCors(); // Enable CORS for serverless compatibility
+//   await app.init();
+  
+//   return server;
+// }
+
+// // Export the server for Vercel to use
+// export const handler = bootstrap();
