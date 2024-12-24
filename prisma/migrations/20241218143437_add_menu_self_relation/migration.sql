6e79c7e050a3b7361af3170c1fdbdfaf12ea89e9
arunakcm <arunak.comdemetrics@gmail.com>
2024-12-24 14:03:17 +0530

    changes

--- prisma/migrations/20241218143437_add_menu_self_relation/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `depth` to the `Menu` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Menu" ADD COLUMN     "depth" INTEGER NOT NULL;
--- prisma/migrations/20241224082656_/migration.sql
+++ prisma/migrations/20241224082656_/migration.sql
@@ -1,11 +1,14 @@
 -- CreateTable
 CREATE TABLE "Menu" (
-    "id" UUID NOT NULL,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "parentId" UUID,
+    "parentId" TEXT,
     "depth" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Menu_pkey" PRIMARY KEY ("id")
 );
+
+-- AddForeignKey
+ALTER TABLE "Menu" ADD CONSTRAINT "Menu_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Menu"("id") ON DELETE SET NULL ON UPDATE CASCADE;
