e3fdc3ef25ad6490f000d0a5eb3296aea38a2110
arunakcm <arunak.comdemetrics@gmail.com>
2024-12-24 12:02:23 +0530

    Configuration changed

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,31 @@
-import { ValidationPipe } from '@nestjs/common';
+// import { ValidationPipe } from '@nestjs/common';
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   // Enable global validation using class-validator
+//   app.useGlobalPipes(new ValidationPipe());
+//   app.enableCors();
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+
+// bootstrap();
+
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  // Enable global validation using class-validator
-  app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  const server = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  app.enableCors(); // Enable CORS for serverless compatibility
+  await app.init();
+  
+  return server;
 }
 
-bootstrap();
+// Export the server for Vercel to use
+export const handler = bootstrap();
